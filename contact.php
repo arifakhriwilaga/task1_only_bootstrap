29915a972e714041c81c2c79eca7fdaca97d25e1
ari_fakhri_wilaga <arifakhri97@gmail.com>
2016-09-07 06:39:56 +0700

    delete table and edit to div

--- contact.php
+++ contact.php
@@ -2,41 +2,34 @@
 include ("app.php");
 
 ?>
-
-   <div class="container">			
-	
-	  <div class="row">
-   		<div class="col-xs-4">
-   		<h2>Ari Fakhri Wilaga</h2>
-   		<img src="img/profile.gif" class="img-circle" width="200px" height="200px" alt="Responsive image">
-	   </div>
-	   <div class="col-xs-8">
-	   <form>
-	   		<table border="0">
- 				<tr class="info">
- 				 <td width="400px">
- 					<label for="exampleInputEmail1"><center>Email</center></label>
- 				 </td>
- 				 <td width="600px">
- 					<input type="email" class="form-control" placeholder="Email">
- 				 </td>
- 				</tr>
- 				<tr> 		
- 				 <td>
- 					<label for="exampleInputEmail1"><vlagin ="top">Message</vlagin></label>
- 				 </td>
- 				 <td>
- 				 	<br>
- 				 	<textarea class="form-control" rows="5" placeholder="Message Anda"></textarea>
- 				 </td>
- 				</tr>
- 				<tr> 
- 				 <td>	
-					<input class="btn btn-default" type="submit" name="click" value="Submit" onclick='alert("for example")'>
- 				 </td>
- 				</tr>				
-			</table>
-		</form>
+   <div class="container">	
+	<div class="row">
+   		<div class="col-xs-3">
+	   		<h2>Ari Fakhri Wilaga</h2>
+	   		<img src="img/profile.gif" class="img-circle" width="200px" height="200px" alt="Responsive image">
 		</div>
-	</div>
+		<div class="col-xs-9">
+			<div class="row">
+				<div class="col-xs-4">
+		 			<label for="exampleInputEmail1"><center>Email</center></label>				
+		 		</div>
+			   <div class="col-xs-8">
+		 			<input type="email" class="form-control" placeholder="Email">
+				</div>
+			<div class="row">
+				<div class="col-xs-12">
+				&nbsp;
+				</div>
+			</div>
+				<div class="col-xs-4">
+					<label for="exampleInputMessage"><center>Message</center></label>
+				</div>
+				<div class="col-xs-8">
+					<textarea class="form-control" rows="5" placeholder="Message Anda"></textarea>
+		 	 	</div>
+				<div class="col-xs-12">	 
+					<input class="btn btn-default" type="submit" name="click" value="Submit" onclick='alert("for example")'>
+		 		</div>
+ 			</div>		
+		</div>
    </div>
